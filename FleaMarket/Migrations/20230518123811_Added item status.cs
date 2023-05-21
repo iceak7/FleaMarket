08343fa61f32f00fe697259a5475e24ba2f3a93e
Isak <isak.jensen@telia.com>
2023-05-21 21:06:33 +0200

    .

---
+++ FleaMarket/Migrations/20230518123811_Added item status.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FleaMarket.Migrations
+{
+    public partial class Addeditemstatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "MarketItems",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "MarketItems");
+        }
+    }
+}
